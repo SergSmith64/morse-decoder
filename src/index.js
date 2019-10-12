c56c64b342d7f927a00d0b1b602a62f8637bb656
SergSmith64 <sergsmith2008@yandex.ru>
2019-10-12 16:51:56 +0400

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -37,9 +37,38 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+function decode(expr) {  
+
+    let i=0, j=0, word = '', letter='', morse='', decode ='';
+  
+    for(i=0; i<expr.length; i+=10){
+        word = expr.slice(i, i+10);
+        for(j=0; j<word.length; j+=2){
+            letter = word[j] + word[j+1];
+    
+            if (letter == '10'){
+                morse += '.'; 
+            }
+             else if (letter == '11'){
+                morse += '-';
+            }
+            else {
+                morse='';
+            }
+            letter = '';
+        }
+        if(morse.length > 0){
+            decode += MORSE_TABLE[morse];
+        }
+        else{
+            decode+= ' ';
+        }
+        morse = '';   
+    
+    }
+    return decode;
+    
+  }
 
 module.exports = {
     decode
